0361c4ec7eff5787320c0ed51bc73de04f8c7b77
Bashistha Joshi <bashsithajoshi108@gmail.com>
2024-09-01 02:03:51 +0200

    Login logout all completed

--- jobportal/src/main/java/jobportal/jobportal/controller/UsersController.java
+++ jobportal/src/main/java/jobportal/jobportal/controller/UsersController.java
@@ -1,11 +1,17 @@
 package jobportal.jobportal.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import jobportal.jobportal.entity.Users;
 import jobportal.jobportal.entity.UsersType;
 import jobportal.jobportal.services.UsersService;
 import jobportal.jobportal.services.UsersTypeService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -49,6 +55,19 @@ public class UsersController {
         return "dashboard";
     }
 
+    @GetMapping("/login")
+    public String login(){
+        return "login";
+    }
+
+    @GetMapping("/logout")
+    public String logout(HttpServletRequest request, HttpServletResponse response){
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if(authentication!=null){
+            new SecurityContextLogoutHandler().logout(request, response, authentication);
+        }
+        return "redirect:/";
+    }
 
 }
 
