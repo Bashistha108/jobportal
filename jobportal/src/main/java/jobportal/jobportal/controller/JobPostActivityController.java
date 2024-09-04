9f8df5d92ea6dc16084f117bf51bdb470dbce26c
Bashistha Joshi <bashsithajoshi108@gmail.com>
2024-09-05 01:18:23 +0200

    recruiter_dashboard

--- jobportal/src/main/java/jobportal/jobportal/controller/JobPostActivityController.java
+++ jobportal/src/main/java/jobportal/jobportal/controller/JobPostActivityController.java
@@ -1,12 +1,15 @@
 package jobportal.jobportal.controller;
 
 import jobportal.jobportal.entity.JobPostActivity;
+import jobportal.jobportal.entity.RecruiterJobsDto;
+import jobportal.jobportal.entity.RecruiterProfile;
 import jobportal.jobportal.entity.Users;
 import jobportal.jobportal.services.JobPostActivityService;
 import jobportal.jobportal.services.UsersService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -14,6 +17,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.Date;
+import java.util.List;
 
 @Controller
 public class JobPostActivityController {
@@ -29,13 +33,18 @@ public class JobPostActivityController {
 
 
 
-    @GetMapping("/dashboard")
+    @GetMapping("/dashboard/")
     public String searchJobs(Model model){
         Object currentUserProfile = usersService.getCurrentUserProfile();
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         if(!(authentication instanceof AnonymousAuthenticationToken)){
             String currentUsername = authentication.getName();
             model.addAttribute("username", currentUsername);
+            if(authentication.getAuthorities().contains(new SimpleGrantedAuthority("Recruiter"))){
+                List<RecruiterJobsDto> recruiterJobs = jobPostActivityService.
+                        getRecruiterJobs(((RecruiterProfile)currentUserProfile).getUserAccountId());
+                model.addAttribute("jobPost", recruiterJobs);
+            }
         }
         model.addAttribute("user", currentUserProfile);
         return "dashboard";
@@ -57,7 +66,7 @@ public class JobPostActivityController {
         jobPostActivity.setPostedDate(new Date());
         model.addAttribute("jobPostActivity", jobPostActivity);
         JobPostActivity saved = jobPostActivityService.addNew(jobPostActivity);
-        return "redirect:/dashboard";
+        return "redirect:/dashboard/";
     }
 }
 
